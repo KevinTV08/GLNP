180eb0997a569bf1978bbb8e3e9296e6cee0e05a
Kevin <kevinntrann@gmail.com>
2018-09-24 13:10:51 -0700

    deployment of site

---
+++ js/main.js
@@ -0,0 +1,37 @@
+let toggleNavStatus = false;
+
+let toggleNav = function() {
+  let getMobileNav = document.querySelector('.mobile-nav');
+  let getMobileNavUl = document.querySelector('.mobile-nav__items');
+  let getMobileNavLinks = document.querySelectorAll('.mobile-nav__item a');
+
+  if (toggleNavStatus === false) {
+    getMobileNav.style.height = '250px';
+    getMobileNav.style.position = 'fixed';
+    getMobileNav.style.zIndex = '1000';
+    getMobileNav.style.top = '50px';
+
+    toggleNavStatus = true;
+  } else if (toggleNavStatus === true) {
+    getMobileNav.style.height = '0';
+    getMobileNav.style.top = '0';
+    getMobileNav.style.position = 'static';
+    toggleNavStatus = false;
+  }
+};
+
+let toggleMobileNav = function() {
+  let getShopBtn = document.querySelector('.dropdown-btn');
+  let getDropDownItems = document.querySelector('.dropdown-items');
+  let getDropDownItem = document.querySelectorAll('.dropdown-item a');
+
+  if (toggleNavStatus === false) {
+    getDropDownItems.style.display = 'block';
+
+    toggleNavStatus = true;
+  } else if (toggleNavStatus === true) {
+    getDropDownItems.style.display = 'none';
+
+    toggleNavStatus = false;
+  }
+};
---
+++ readme.md
@@ -0,0 +1,17 @@
+## Great Leaf National Park
+
+![screenshot](img/screenshot.png)
+
+An tourism website for madeup national park made using HTML5, Flexbox, CSS3, Sass/SCSS preprocessor, and JavaScript.
+
+## Built With
+
+- HTML5
+- Flexbox
+- CSS3
+- Sass/SCSS
+- JavaScript
+
+## Acknowledgement
+
+The card feature design and other components of this projects was inspired by Jonas Schmedtmann.
